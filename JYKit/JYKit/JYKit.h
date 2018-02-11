683bb504a8b3b8539032354363f98baed0b19c7a
LongJunYan <longjunyan913@gmail.com>
2018-02-11 10:30:31 +0800

    创建版本 添加gitignore

---
+++ JYKit/JYKit/JYKit.h
@@ -0,0 +1,19 @@
+//
+//  JYKit.h
+//  JYKit
+//
+//  Created by LongJunYan on 2018/2/11.
+//  Copyright © 2018年 onlecat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JYKit.
+FOUNDATION_EXPORT double JYKitVersionNumber;
+
+//! Project version string for JYKit.
+FOUNDATION_EXPORT const unsigned char JYKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JYKit/PublicHeader.h>
+
+
